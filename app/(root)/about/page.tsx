0f7b5071c3b778fb767f13353d2bc1d89d74c511
BegiDev <abdukhamidov0904@gmail.com>
2024-08-09 15:23:50 +0500

    Error fix

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,32 +1,7 @@
-import { Badge } from '@/components/ui/badge'
-import { learningTechnology } from '@/constant'
-
 function AboutMe() {
 	return (
 		<div className='container max-w-full md:max-w-[670px] mx-auto mt-[85px]'>
-			<h1 className='text-center text-4xl font-normal'>Men haqimda</h1>
-			<div className='border-t mt-6 font-sourceSans font-normal leading-7'>
-				<h3 className='mt-4 text-base'>
-					Hammaga salom, mening ismim Begzod. Jizzax shahrida 200*&apos;-yil
-					tug&apos;ilganman. Hozirda Front-end dasturchiman, dasturlashga
-					2022-yil kuz oylarida kirib kelganman. Dasturlashni self-study
-					o&apos;rganganman va o&apos;rganib kelmoqdaman.
-				</h3>
-				<div className='mt-5'>
-					<h4 className='font-medium '>O&apos;rgangan texnalogyalarim:</h4>
-					<div className='mt-2'>
-						{learningTechnology.map(item => (
-							<Badge
-								variant={'destructive'}
-								key={item.name}
-								className='mr-2 px-3 py-1'
-							>
-								{item.name}
-							</Badge>
-						))}
-					</div>
-				</div>
-			</div>
+			<h1 className='text-center text-4xl font-normal'>Soon...</h1>
 		</div>
 	)
 }
